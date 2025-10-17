70aafbdce52444ea545fd65286cb5fbb037bd61e
Harwin <sandhuharwinkaur@gmail.com>
2025-10-17 17:06:23 +1100

    learn to make form in react

--- src/MyForm.js
+++ src/MyForm.js
@@ -1,8 +1,28 @@
+import { useRef } from "react";
+
+
 export const MyForm = () => {
+
+    const addressRef = useRef("");
+
+    const handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log(addressRef.current.value);
+};
+
+const handleOnChange = (e) => {
+    const str = e.target.value;
+    console.log(str);
+};
+
+console.log(addressRef);
+
     return (
-    <form action="" className="card">
+    <form action="" className="card" onSubmit={handleOnSubmit}>
         <div className="mb-3">
-            <input type="text" className="form-control"/>
+            <input type="text" className="form-control mb-3" required placeholder="name" onChange={handleOnSubmit}/>
+
+            <input type="text" className="form-control" required placeholder="address" ref={addressRef}/>
         </div>
         <button className="btn btn-primary">Submit</button>
     </form>
